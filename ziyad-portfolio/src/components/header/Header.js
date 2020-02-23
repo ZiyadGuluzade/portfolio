657cf5fc8ff6374dfa86b52f9ccc349f3e37c5fe
Z.Guluzade <ziyad_guluzade@outlook.com>
2020-02-23 16:31:50 -0500

    updated file structure

---
+++ ziyad-portfolio/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Navbar from '../Navbar';
+import keyboard from '../style/images/keyboard.jpg';
+
+
+class Header extends React.Component {
+    render() {
+        const background = keyboard;
+        return(
+            <React.Fragment>
+                <div className='header' style={{backgroundImage: `url(${background})`,
+                                                backgroundSize: 'cover',
+                                                height: '700px'}}>
+                    <Navbar />
+                    <h1 className='name' style={{ padding: 30, color: '#001321'}}>ZIYAD GULUZADE</h1>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Header;
--- ziyad-portfolio/src/components/navbar/Navbar.js
+++ ziyad-portfolio/src/components/navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Link from './Link';
+import Link from './link/Link';
 import '../style/Navbar.css';
 
 class Navbar extends React.Component {
     render() {
         return(
             <React.Fragment>
-                <div className='navbar' style={{backgroundColor: '#2E5877', color: 'black', padding: 20}}>
+                <div className='navbar' style={{border: '2px solid black', 
+                                                color: 'black',
+                                                padding: 20,
+                                                height: '100px'}}>
                     <Link linkName='About' link='' />
                     <Link linkName='Projects' link='' />
                     <Link linkName='Contacts' link='' />
