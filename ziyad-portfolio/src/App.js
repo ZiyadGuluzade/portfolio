d578793472d90486ec6dcae4239e92087640c6f2
Z.Guluzade <ziyad_guluzade@outlook.com>
2020-02-26 01:05:52 -0500

    changed the title

--- ziyad-portfolio/src/App.js
+++ ziyad-portfolio/src/App.js
@@ -14,14 +14,12 @@ class App extends React.Component {
   render() {
   
     return (
-      <div className="App">
-        <div className='container'>
-          <Header />
-          <Aboutme />
-          <Projects />
-          <Contacts />
-          <Footer />
-        </div>
+      <div className='container'>
+        <Header />
+        <Aboutme />
+        <Projects />
+        <Contacts />
+        <Footer />
       </div>
     );
   }
