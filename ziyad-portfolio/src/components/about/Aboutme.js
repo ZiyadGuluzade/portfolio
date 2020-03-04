a21e56f3ec2a44ecd566fb1f98cfff2e6cd89760
Z.Guluzade <ziyad_guluzade@outlook.com>
2020-03-04 18:28:01 -0500

    added simple spacing to aboutme component

--- ziyad-portfolio/src/components/about/Aboutme.js
+++ ziyad-portfolio/src/components/about/Aboutme.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './aboutme.css';
 
 class Aboutme extends React.Component {
     render() {
         return(
             <React.Fragment>
-                <section id='about'>
-                    <div className='block'>
-                        <h2 className='subtitle'>About Me Component</h2>
-                        <p>Hi. I am a Full Stack Web Developer. I am passionate about building responsive and well-designed web applications. That is what I do. That is what I am all about.
+                <section id='about' className='section subtitle is-4'>
+                    <div className='about'>
+                        <h2 className='subtitle has-text-centered is-2' id='about-subtitle'>A BIT ABOUT ME</h2>
+                        <p className='about-text'>Hi, I am Ziyad.<br></br>I am Full Stack Web Developer with a background in Hotel Business, based in Toronto ON, Canada. responsive and well-designed web applications. That is what I do. That is what I am all about.
                         Throughout my education in University of Toronto, I have developed such skills as problem solving, working well under pressure, critical thinking, and collaborating with teams. Gaining experience in such a sensitive and dynamic workspace helped me to step up and chase my dream of being a developer. Mastering technologies like Javascript, Node.js and React.js helped me to truly understand core base of development. The complexity and endless varieties of opportunities of this industry attract and make me passionate about technology. Self-development and desire to learn are the main forces pushing me forward into the field and helping me to attain my highest goals.</p>
                     </div>
                 </section>
--- ziyad-portfolio/src/components/link/Link.js
+++ ziyad-portfolio/src/components/link/Link.js
@@ -4,9 +4,10 @@ import './link.css';
 class Link extends React.Component {
     render() {
         const name = this.props.name;
+        const href = this.props.href;
         return(
             <React.Fragment>
-                <a className="button navbar-item tablet" href=''>
+                <a className="button navbar-item tablet">
                     {name}
                 </a>
             </React.Fragment>
