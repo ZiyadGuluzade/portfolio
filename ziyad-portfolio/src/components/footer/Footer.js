7d7b258d590af4184cfc5b0618ff00bfef4b12f3
Z.Guluzade <ziyad_guluzade@outlook.com>
2020-03-09 16:04:27 -0400

    completed footer

--- ziyad-portfolio/src/components/footer/Footer.js
+++ ziyad-portfolio/src/components/footer/Footer.js
@@ -7,8 +7,8 @@ class Footer extends React.Component {
         return(
             <React.Fragment>
                 <div className='footer has-text-centered'>
-                    <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" id="bulma" height="24" />
-                    <div className='content has-text-centered subtitle is-4' id='footer-title'>Copyright 2020 <i className='fa fa-copyright'></i> Ziyad Guluzade | All Right Reserved</div>
+                    <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="80" id="bulma" height="15" />
+                    <div className='content has-text-centered subtitle is-4' id='footer-title'>Copyright 2020 <i className='fa fa-copyright'></i> - Ziyad Guluzade</div>
                 </div>
             </React.Fragment>
         )
